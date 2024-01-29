c8092c49d1fbc43dd076a82d000f699efc1577aa
Rupali230 <113345477+Rupali230@users.noreply.github.com>
2024-01-29 13:20:11 +0530

    Update MyProfileForm.java

--- projectORS/src/main/java/com/rays/form/MyProfileForm.java
+++ projectORS/src/main/java/com/rays/form/MyProfileForm.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.Pattern;
 
 /**
  * Contains MyProfile for elements and its declarative validation
- * @author Basant Vishwakarma
+ * @author Rupali patel
  */
 
 public class MyProfileForm {
