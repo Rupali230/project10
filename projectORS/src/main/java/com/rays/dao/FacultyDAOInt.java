51fee417c41aa5ab424000c5dc02f9b6ce50001e
Rupali230 <113345477+Rupali230@users.noreply.github.com>
2024-01-29 13:07:24 +0530

    Update FacultyDAOInt.java

--- projectORS/src/main/java/com/rays/dao/FacultyDAOInt.java
+++ projectORS/src/main/java/com/rays/dao/FacultyDAOInt.java
@@ -3,7 +3,7 @@ package com.rays.dao;
 import com.rays.common.BaseDAOInt;
 import com.rays.dto.FacultyDTO;
 /**
- * @author Basant Vishwakarma
+ * @author Rupali patel
  *
  */
 public interface FacultyDAOInt extends BaseDAOInt<FacultyDTO> {
