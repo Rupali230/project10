d4712f8c344d2a8d5f90576a471e6ce8f39ad694
Rupali230 <113345477+Rupali230@users.noreply.github.com>
2024-01-29 13:46:29 +0530

    Update AttachmentServiceImpl.java

--- projectORS/src/main/java/com/rays/common/attachment/AttachmentServiceImpl.java
+++ projectORS/src/main/java/com/rays/common/attachment/AttachmentServiceImpl.java
@@ -18,7 +18,7 @@ import com.rays.common.UserContext;
  * back.
  * 
  * Default propagation value is Propagation.REQUIRED and readOnly = false
- * @author Basant Vishwakarma
+ * @author Rupali patel
  */
 @Service
 @Transactional
