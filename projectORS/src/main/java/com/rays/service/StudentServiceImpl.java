0835731e471a7d7d3835632a7e726578df54f8c5
Rupali230 <113345477+Rupali230@users.noreply.github.com>
2024-01-29 13:24:39 +0530

    Update StudentServiceImpl.java

--- projectORS/src/main/java/com/rays/service/StudentServiceImpl.java
+++ projectORS/src/main/java/com/rays/service/StudentServiceImpl.java
@@ -19,7 +19,7 @@ import com.rays.exception.DuplicateRecordException;
  * back.
  * 
  * Default propagation value is Propagation.REQUIRED and readOnly = false
- * @author Basant Vishwakarma
+ * @author Rupali patel
  */
 @Service
 @Transactional
