fd3debf50d5ee37afa0157f83bcf7077810e3ba4
Rupali230 <113345477+Rupali230@users.noreply.github.com>
2024-01-29 13:11:51 +0530

    Update TimeTableDAOInt.java

--- projectORS/src/main/java/com/rays/dao/TimeTableDAOInt.java
+++ projectORS/src/main/java/com/rays/dao/TimeTableDAOInt.java
@@ -3,7 +3,7 @@ package com.rays.dao;
 import com.rays.common.BaseDAOInt;
 import com.rays.dto.TimeTableDTO;
 /**
- * @author Basant Vishwakarma
+ * @author Rupali patel
  *
  */
 public interface TimeTableDAOInt extends BaseDAOInt<TimeTableDTO> {
