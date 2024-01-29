7d4c9a76f803c9c03e84ea821e787717a38cf9b5
Rupali230 <113345477+Rupali230@users.noreply.github.com>
2024-01-29 13:41:25 +0530

    Update EmailDTO.java

--- projectORS/src/main/java/com/rays/common/mail/EmailDTO.java
+++ projectORS/src/main/java/com/rays/common/mail/EmailDTO.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 
 /**
  * Contains email subject, body and attached files to be sent
- * @author Basant Vishwakarma
+ * @author Rupali patel
  *
  */
 public class EmailDTO {
