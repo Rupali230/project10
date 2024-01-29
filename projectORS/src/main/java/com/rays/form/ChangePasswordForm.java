6a6fe2974e24a393dcf9540cddacbd6ff24332f1
Rupali230 <113345477+Rupali230@users.noreply.github.com>
2024-01-29 13:17:45 +0530

    Update ChangePasswordForm.java

--- projectORS/src/main/java/com/rays/form/ChangePasswordForm.java
+++ projectORS/src/main/java/com/rays/form/ChangePasswordForm.java
@@ -7,7 +7,7 @@ import com.rays.common.BaseForm;
 
 /**
  * Contains change password form elements and their declarative input validations.
- * @author Basant Vishwakarma
+ * @author Rupali patel
  */
 
 public class ChangePasswordForm extends BaseForm {
